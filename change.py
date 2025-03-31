b685da0cc1ce9659d97b872e33a2cf4d487fcc08
jeronimo correa <jcorrea@mail.austral.edu.ar>
2025-03-31 13:09:11 -0300

    Entrega de tp 2

--- change.py
+++ change.py
@@ -1,3 +1,9 @@
 def change():
     expense = 23.75
     money = 100
+    change = money - expense
+    pesos = int(change)
+    cents = int((change - pesos) * 100)
+    print(f'Ingresar gasto:\n{expense}\nDinero recibido\n{money}\n\nVuelto\n\nPesos:\n{pesos}\nCentavos:\n{cents}')
+
+change()
